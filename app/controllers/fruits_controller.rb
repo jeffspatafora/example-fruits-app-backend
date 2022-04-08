3ce0510d0bb0e7622d56ee00d191cc0990911a7f
jeffspatafora <jeffspatafora@gmail.com>
2022-04-07 23:57:48 -0700

    adds create action

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -5,4 +5,16 @@ class FruitsController < ApplicationController
     render json: fruits.as_json
   end
 
+  def create
+    fruit = Fruit.new(
+      kind: params[:kind],
+      name: params[:name],
+      quantity: params[:quantity],
+      image: params[:image]
+    )
+
+    fruit.save
+    render json: fruit.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/fruits" => "fruits#index"
+  post "/fruits" => "fruits#create"
 end
